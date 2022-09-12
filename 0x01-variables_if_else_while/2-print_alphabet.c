67f1520c998a8f5289cb63717edae00ff8e8f00a
DennisGicheru <dennisgicheru698@gmail.com>
2022-09-12 13:07:18 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Return: 0(correct)
+*/
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+
+return (0);
+}
