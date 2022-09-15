ed46b1e12b4818432a8dc837dd2cf0a8c5042b77
DennisGicheru <dennisgicheru698@gmail.com>
2022-09-15 15:02:04 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_alphabet - print the alphabet in lowercase
+*
+* return : void
+*/
+
+
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
