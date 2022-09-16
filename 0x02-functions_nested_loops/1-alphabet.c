7c6ca5397a66f4f00cdb7f0d60b7e9074fd61388
DennisGicheru <dennisgicheru698@gmail.com>
2022-09-15 22:08:31 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
 * print_alphabet - print the alphabet in lowercase
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,8 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
-/*
- * print_str -prints out a string
- *
- * return: c string
-*/
 
 int _putchar(char c);
+
+
 #endif 
