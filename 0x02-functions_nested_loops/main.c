06e389776404b0bb76b17dd6c3a3ec96cbf66630
DennisGicheru <dennisgicheru698@gmail.com>
2022-09-15 22:11:20 -0700

    task 1

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- *  * main - check the code for Holberton School students.
- *   *
- *    * Return: Always 0.
- *     */
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
 int main(void)
 {
 		print_alphabet();
