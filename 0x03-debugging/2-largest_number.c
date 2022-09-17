dcff0113318f928637bab69a7708beafcfefad5c
DennisGicheru <dennisgicheru698@gmail.com>
2022-09-16 20:13:13 -0700

    Task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,6 +10,7 @@
 
 int largest_number(int a, int b, int c)
 {
+
 int largest;
 
 if (a >= b && a >= c)
@@ -22,7 +23,7 @@ largest = b;
 }
 else
 {
-										largest = c;
-										}
+largest = c;
+}
 return (largest);
 }
