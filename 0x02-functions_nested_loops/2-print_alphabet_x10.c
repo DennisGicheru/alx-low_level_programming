a5bd648742d64f8360ead32c6f7cd1dd14644a04
DennisGicheru <dennisgicheru698@gmail.com>
2022-09-21 18:40:43 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+
+#include "main.h"
+
+/**
+ *  * print_alphabet_x10 - prints alphabet 10 times
+ *   *
+ *    * Return: always 0
+*/
+
+
+void print_alphabet_x10(void)
+{
+	int i, a;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (a = 97; a <= 122; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+}
