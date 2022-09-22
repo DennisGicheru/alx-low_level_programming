ff09d5da14dbaaafef96c347e5764792c248106b
DennisGicheru <dennisgicheru698@gmail.com>
2022-09-21 22:41:19 -0700

    Task 9-print 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/*
+ * times_tablev-prints the 9 times table
+ *
+ * Return: no return
+ */
+
+void print_alphabet(void)
+{
+int i;
+for (i = 97; i < 123; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
