7ee8da3e3079baee7588fa1d29d0730e4e440d9d
DennisGicheru <dennisgicheru698@gmail.com>
2022-09-21 22:51:13 -0700

    Task 9-print 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,13 +5,28 @@
  * Return: no return
  */
 
-
-void print_alphabet(void)
+void times_table(void)
 {
-int i;
-for (i = 97; i < 123; i++)
+int a, b, c;
+for (a = 0; a <= 9; a++)
 {
-_putchar(i);
+_putchar(48);
+for (b = 1; b <= 9; b++)
+{
+c = a * b;
+_putchar(44);
+_putchar(32);
+if (c <= 9)
+{
+_putchar(32);
+_putchar(c + 48);
+}
+else
+{
+_putchar((c / 10) + 48);
+_putchar((c % 10) + 48);
+}
 }
 _putchar('\n');
+}
 }
