e6969ed27d27a6e7c2ba46832f1f297e5cbefe0b
DennisGicheru <dennisgicheru698@gmail.com>
2022-09-24 00:54:44 -0700

    more c functions nested loops

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+int main(void);
+void print_traingle(int size);
+#endif
