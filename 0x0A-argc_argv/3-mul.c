cb5bab2dfc72e8b34cc0d3c41d363b61f81c9ea8
DennisGicheru <dennisgicheru698@gmail.com>
2022-10-03 10:31:41 -0700

    argc, argv-corrections

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,16 +10,17 @@
 
 int main(int argc, char *argv[])
 {
-	int num_1, num_2, mul;
+int a, b, result;
 
-	if (argc != 3)
-		printf("Error\n");
-	else
-	{
-		num_1 = atoi(argv[1]);
-		num_2 = atoi(argv[2]);
-		mul = num_1 * num_2;
-		printf("%d/n", mul);
-	}
-	return (0);
+if (argc <= 2)
+{
+printf("Error\n");
+return (1);
+}
+/* atoi convert a string to an int */
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+result = a * b;
+printf("%d\n", result);
+return (0);
 }
