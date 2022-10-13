627612845f6956bd866bb8ece9d732063b31e1cb
DennisGicheru <dennisgicheru698@gmail.com>
2022-10-13 02:20:46 -0700

    tasks 2 and 3

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of compilation file followed by new line
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("%s/n", __BASE_FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef ABSOLUTEVAL
+#define ABSOLUTEVAL
+#define ABS(x) ((x) < 0 ? ((x) * (-1)) : (x))
+#endif
