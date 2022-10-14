c717e55843afad096f21c16e14efe0df4ff50c4b
DennisGicheru <dennisgicheru698@gmail.com>
2022-10-14 07:30:44 -0700

    function pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - function that prints a name.
+ *@array: array
+ *    *@size: size of the array
+ *@action:callback function
+ * Description: function that prints a name
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array != NULL && action != NULL)
+
+	for (i = 0; i < size; i++)
+	{
+		action(*array);
+		array++;
+	}
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,4 @@
+Function Pointers
+Learning what are function pointers and how to use them
+What do function pointers exactly hold
+Where does a function pointer point to in the memory
