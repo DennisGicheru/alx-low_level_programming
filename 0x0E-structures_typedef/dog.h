83ada4063d4e481f92ad8ab5a56e56c757c096a5
DennisGicheru <dennisgicheru698@gmail.com>
2022-10-13 21:52:46 -0700

    task 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,8 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}dog_s;
+} dog_s;
+int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
