833d3d770f28bddadeb9e0c7926830847670e4f4
DennisGicheru <dennisgicheru698@gmail.com>
2022-10-13 21:18:59 -0700

    task 0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+C-structures, typedef
+What are structures, when, why and how to use them
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * struct dog - Dogs information
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner's name
+ *
+ * Description: General information abaout a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
