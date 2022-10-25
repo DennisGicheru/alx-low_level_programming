4aa300dc2c7c54d456b84c636e7b1be0c843aaf3
DennisGicheru <dennisgicheru698@gmail.com>
2022-10-25 10:33:08 -0700

    Singly-linked-lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,7 +7,7 @@
  * Return: number of nodes in list
  */
 
-size_t print_list(const list_h *h)
+size_t print_list(const list_t *h)
 {
 	size_t n = 0;
 
@@ -17,7 +17,7 @@ size_t print_list(const list_h *h)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-			n++;
+		n++;
 		h = h->next;
 	}
 
