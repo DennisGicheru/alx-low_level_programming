bcad382b127a8ed812a5f407afa1bd375a17b31c
DennisGicheru <dennisgicheru698@gmail.com>
2022-10-25 10:40:28 -0700

    Singly-linked-lists

--- 0x12-singly_linked_lists/main.h
+++ 0x12-singly_linked_lists/main.h
@@ -22,7 +22,9 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
-char *insert(char *str, unsigned int len, list_t *h);
-void delete(char *str, list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
