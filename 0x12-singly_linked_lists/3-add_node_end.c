c1d89bd6a031499a1578768924ac9c444d2f5d3b
DennisGicheru <dennisgicheru698@gmail.com>
2022-10-26 07:37:54 -0700

    singly-linked-lists

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,44 +9,46 @@
 
 int _strlen(const char *s)
 {
-	int n;
+	int i;
 
-	n = 0;
-
-	while (s[n] != '\0')
+	i = 0;
+	while (s[i] != '\0') /*Count character of string*/
 	{
-		n++;
+		i++;
 	}
-	return (n);
+
+	return (i);
 }
 
 /**
-* add_node_end -add node to end of a list
-* @head: pointer to head
-*@str: string
-*Return: address of new element
-*/
-
+ * add_node_end -  adds a new node at the end of a list
+ * @head: pointer
+ *@str:string
+ * Return:  address of the new element
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node, *last_node = *head;
-	/* using malloc */
+	/*Using malloc*/
 	new_node = malloc(sizeof(list_t));
+
 
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->str = strdup(str);
+	new_node->str = strdup(str); /*Assing data*/
 	new_node->len = _strlen(str);
 	new_node->next = NULL;
 
 	if (*head == NULL)
 	{
-		*head = NULL;
+		*head = new_node;
 		return (*head);
 	}
 	while (last_node->next != NULL)
 		last_node = last_node->next;
+
 	last_node->next = new_node;
+
 	return (*head);
 }
