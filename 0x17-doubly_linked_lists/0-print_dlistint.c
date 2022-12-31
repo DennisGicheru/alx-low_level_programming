fc61eecd523b9bad8760dd863a2d353917c99ffb
DennisGicheru <dennisgicheru698@gmail.com>
2022-12-31 09:37:47 -0800

    task 0-edit

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
-* print_dlistint - print doubly linked list
+* print_dlistint-prints a doubly linked list
 * @h: pointer to the header
-* Return - number of nodes
+* Return: number of nodes
 */
 
 size_t print_dlistint(const dlistint_t *h)
