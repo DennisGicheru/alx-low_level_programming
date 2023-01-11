c4579d589434850b34767b488c430c828f8602de
DennisGicheru <dennisgicheru698@gmail.com>
2023-01-11 13:07:07 -0800

    task 6-fixing bugs

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -11,7 +11,7 @@ void hash_table_delete(hash_table_t *ht)
 	unsigned long int i;
 	hash_node_t *node;
 
-	if (ht == NULL || ht->array == NULL || !ht->size)
+	if (!ht || !ht->array || !ht->size)
 		return;
 	for (i = 0; i <= ht->size; i++)
 	{
