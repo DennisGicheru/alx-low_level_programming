de00b0bbbd59c13ea34b2008f9884779d1ed322d
DennisGicheru <dennisgicheru698@gmail.com>
2023-09-30 20:50:59 +0300

    advanced task 2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*  *main- entry point
+* prints exactly "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+* and then a new line and returns to the stderr"
+* *Return: always 0 (success)
+*/
+
+int main(void)
+{
+printf("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+return (0);
+}
