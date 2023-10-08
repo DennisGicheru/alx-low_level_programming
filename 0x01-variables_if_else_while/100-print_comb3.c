2c631545d9eeeca1603dfc0ae1d7b6f9a3e1727d
DennisGicheru <dennisgicheru698@gmail.com>
2023-10-08 20:26:55 +0300

    advanced task 1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+** main - Entry point
+**
+** Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int d;
+int n;
+
+for (d = '0'; d <= '9'; d++)
+{
+for (n = '0'; n <= '9'; n++)
+{
+if (d != n && d < n)
+{
+putchar(d);
+putchar(n);
+putchar (',');
+putchar (' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
